9e8323016d5b656212ccd2dd0581dc9f8d734be1
Kumar Potnuri <potnurikumar@gmail.com>
2020-06-28 11:12:46 +0530

    added change observer and handling authorization status code for restricted and denied

--- MultiSelectPicker/ViewController.swift
+++ MultiSelectPicker/ViewController.swift
@@ -37,15 +37,52 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         
-        photoListTable.dataSource = self
+        self.photoListTable.dataSource = self
+        fetchingPhotoOptions()
         
+        photoAblumAuthorizationStatus()
+        
+        PHPhotoLibrary.shared().register(self)
+    }
+    
+    deinit {
+        PHPhotoLibrary.shared().unregisterChangeObserver(self)
+    }
+    
+    func fetchingPhotoOptions() {
         let allPhotosOptions = PHFetchOptions()
         allPhotosOptions.sortDescriptors = [NSSortDescriptor(key: "creationDate", ascending: true)]
         allPhotos = PHAsset.fetchAssets(with: allPhotosOptions)
         smartAlbums = PHAssetCollection.fetchAssetCollections(with: .smartAlbum, subtype: .albumRegular, options: nil)
         userCollections = PHCollectionList.fetchTopLevelUserCollections(with: nil)
+    }
+    
+    func photoAblumAuthorizationStatus() {
+        
+        PHPhotoLibrary.requestAuthorization { (status) in
+            if status == .authorized {
+                self.fetchingPhotoOptions()
+                DispatchQueue.main.async {
+                    self.photoListTable.reloadData()
+                }
+                
+            } else if status == .denied || status == .restricted {
+                
+                DispatchQueue.main.async {
+                    let alert = UIAlertController(title: "Photo Library", message: "Photo Library access is needed to save images and video", preferredStyle: .alert)
+
+                    // Add "OK" Button to alert, pressing it will bring you to the settings app
+                    alert.addAction(UIAlertAction(title: "Settings", style: .default, handler: { action in
+                      UIApplication.shared.open(URL(string: UIApplication.openSettingsURLString)!)
+                    }))
+                    
+                    alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+                    // Show the alert with animation
+                    self.present(alert, animated: true)
+                }
+            }
+        }
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -116,6 +153,36 @@ extension ViewController : UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
         return sectionLocalizedTitles[section]
+    }
+}
+
+// MARK: PHPhotoLibraryChangeObserver
+
+extension ViewController: PHPhotoLibraryChangeObserver {
+    /// - Tag: RespondToChanges
+    func photoLibraryDidChange(_ changeInstance: PHChange) {
+        
+        // Change notifications may originate from a background queue.
+        // Re-dispatch to the main queue before acting on the change,
+        // so you can update the UI.
+        DispatchQueue.main.sync {
+            // Check each of the three top-level fetches for changes.
+            if let changeDetails = changeInstance.changeDetails(for: allPhotos) {
+                // Update the cached fetch result.
+                allPhotos = changeDetails.fetchResultAfterChanges
+                // Don't update the table row that always reads "All Photos."
+            }
+            
+            // Update the cached fetch results, and reload the table sections to match.
+            if let changeDetails = changeInstance.changeDetails(for: smartAlbums) {
+                smartAlbums = changeDetails.fetchResultAfterChanges
+                photoListTable.reloadSections(IndexSet(integer: Section.smartAlbums.rawValue), with: .automatic)
+            }
+            if let changeDetails = changeInstance.changeDetails(for: userCollections) {
+                userCollections = changeDetails.fetchResultAfterChanges
+                photoListTable.reloadSections(IndexSet(integer: Section.userCollections.rawValue), with: .automatic)
+            }
+        }
     }
 }
 
